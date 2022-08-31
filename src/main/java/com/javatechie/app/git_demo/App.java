57de00af7a1e725373f627c1b571084948ab96f5
khuddussk <skkhuddussk40@gmail.com>
2022-08-31 13:27:52 +0530

    added employeee class & added list of employee & displaying emp data using for each

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -1,5 +1,11 @@
 package com.javatechie.app.git_demo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.javatechie.app.git_demo.Employee;
+
 /**
  * Hello world!
  *
@@ -10,6 +16,26 @@ public class App
     {
         System.out.println( "Hello Java Techie App Welcome to  Git Demo...!" );
         System.out.println( "Changes done after commiting to locl repo....!" );
+        
+        //adding new changes 
+        
+        System.out.println("Emplist...");
+
+		List<Employee> employeeList = new ArrayList<Employee>();
+
+		
+		
+		employeeList.add(new Employee(1, "Khddus Shaikh", 33, "Male", "DEVELOPEMNT", 2019, 17500.00, Arrays.asList("project 1","project 2")));
+		employeeList.add(new Employee(2, "Salman Ahmed",  32, "Male", "STORAGE & DATA BACKUP ", 2019, 50000.00, Arrays.asList("project 3","project 4")));
+		employeeList.add(new Employee(3, "Aleem Sayyed",  31, "Male", "DATABASE TEAM", 2019, 35000.00, Arrays.asList("project 5","project 6")));
+
+		
+
+		//1.foreach
+		 employeeList.forEach(System.out::println);
+			System.out.println("======================================================================================");
+
+
 
     }
 }
---
+++ src/main/java/com/javatechie/app/git_demo/Employee.java
@@ -0,0 +1,96 @@
+package com.javatechie.app.git_demo;
+
+import java.util.List;
+
+public class Employee {
+	private int id;
+	private String name;
+	private int age;
+	private String gender;
+	private String department;
+	private int yearOfJoining;
+	private double salary;
+	private  List<String> projects;
+	
+	
+	public Employee(int id, String name, int age, String gender, String department, int yearOfJoining, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.department = department;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+	}
+	
+	public Employee(int id, String name, int age, String gender, String department, int yearOfJoining, double salary,
+			List<String> projects) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.department = department;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+		this.projects = projects;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public int getYearOfJoining() {
+		return yearOfJoining;
+	}
+	public void setYearOfJoining(int yearOfJoining) {
+		this.yearOfJoining = yearOfJoining;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public List<String> getProjects() {
+		return projects;
+	}
+	public void setProjects(List<String> projects) {
+		this.projects = projects;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + ", projects=" + projects
+				+ "]";
+	}
+	
+	
+}
